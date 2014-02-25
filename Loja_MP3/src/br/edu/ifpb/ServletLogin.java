1a2bb6027a1f28497d3417e4cc3223a315e16f78
LenildoSousa <lenildo2@gmail.com>
2014-02-25 18:19:50 -0300

    Alterações, criado ServletLogin.java, arquivo senhas, arquivo erro.html, arquivo Cadastro.html e Login.html. Incompleto ainda!

---
+++ Loja_MP3/src/br/edu/ifpb/ServletLogin.java
@@ -0,0 +1,55 @@
+package br.edu.ifpb;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet("/servletlogin.do")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private Properties mapa;
+    
+    public void init() throws ServletException
+    {
+    	this.mapa = new Properties();
+    	
+    	try
+    	{
+    		    		
+    		String arquivo = this.getServletContext().getInitParameter("caminho_arquivo_senhas");
+    		
+    		this.mapa.load(this.getServletContext().getResourceAsStream(arquivo));
+    	    		
+    	} catch (IOException e)
+    	{
+    		e.printStackTrace();
+    		
+    	}
+	}
+    
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login = request.getParameter("edtLogin");
+		String senha = request.getParameter("edtSenha");
+		
+		if (this.validarSenha (login,senha)) {
+			response.sendRedirect("...");
+		} else response.sendRedirect("erro.html");
+			
+	}//doPost
+	
+	private boolean validarSenha(String login, String senha)
+	{
+	return this.mapa.getProperty(login).equals(senha);
+	
+	}
+
+}
